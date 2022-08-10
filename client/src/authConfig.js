8c74f472257944e71eec35058f9a42298ed9240c
Logan Barsell <loganbarsell@MacBook-Pro.local>
2022-08-10 13:35:37 -0700

    authconfig

--- client/src/authConfig.js
+++ client/src/authConfig.js
@@ -1,6 +1,6 @@
 export const msalConfig = {
   auth: {
-    clientId: process.env.REACT_APP_CLIENT_ID,
+    clientId: 'ee2c531a-b402-4bd5-ab01-df3ff3900ab0',
     authority: 'https://login.microsoftonline.com/5184daaf-16a2-479b-8174-79d2e7257401', // This is a URL (e.g. https://login.microsoftonline.com/{your tenant ID})
     redirectUri: 'https://yesdevil.com',
   },
