fcf8de20c00ad7492763ab8f182b9efed6887431
Logan Barsell <loganbarsell@MacBook-Pro.local>
2022-08-26 20:53:23 -0700

    symbolic link fix

--- client/src/pages/Merch/Products.js
+++ client/src/pages/Merch/Products.js
@@ -20,7 +20,6 @@ const Products = ({ fetchProducts, products }) => {
   const renderProducts = products.length ? () => 
       products.map((item, index) => {
         const { product, price } = item;
-        // const addedToCart = cart.products && cart.products.some(cartItem => cartItem.product.id === product.id);
         return (
           <div key={index} className="col-lg-4 col-md-6">
             <div className="card product"><img className="card-img-top" src={product.images[0]} alt="product" />
@@ -30,16 +29,6 @@ const Products = ({ fetchProducts, products }) => {
   
                 <div className="price">$<span>{price.unit_amount / 100}</span></div>
                 <div className="d-grid gap-2">
-                {/* { addedToCart ?
-                  <button
-                    className="btn btn-sm btn-danger"
-                    disabled
-                  >Added to Cart &nbsp;
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-check-lg" viewBox="0 0 16 16">
-                    <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"/>
-                  </svg>
-                  </button>
-                : */}
                   <button
                     onClick={() => addToCart(item)}
                     className="btn btn-sm btn-danger"
